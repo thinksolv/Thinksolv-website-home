ffeb8cb55839a869425a714a582c292ec187aff8
SamsundarR <142050188+SamsundarR@users.noreply.github.com>
2024-11-23 13:14:52 +0530

    Initial Commit

---
+++ src/types/feature.ts
@@ -0,0 +1,9 @@
+import { IconType } from "react-icons";
+
+export type Feature = {
+  id: number;
+  icon: IconType;
+  title: string;
+  description: string;
+  url: string;
+};
---
+++ src/types/service.ts
@@ -0,0 +1,7 @@
+export type Service = {
+  id: number;
+  title: string;
+  description: string;
+  image: string; // This will hold the path to the image (instead of icon)
+  url: string; // Link for the service detail page or product page
+};
