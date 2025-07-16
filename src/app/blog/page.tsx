cc877e7a9dcbed36d82861b049b5aae7f1ee8901
Vikram Aruchamy <vikram@docstomarkdown.pro>
2025-07-16 17:05:32 +0530

    test code committed for building

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -8,7 +8,7 @@ import { Metadata } from "next";
 export const metadata: Metadata = {
   title: "Blog Page - Solid SaaS Boilerplate",
   description: "This is Blog page for Solid Pro",
-  // other metadata
+  // other metadata -add as necessary
 };
 
 const BlogPage = async () => {
